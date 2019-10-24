26bbe81b413783a23e416b3c6a6c0fa6a6d27acc
miara1502 <miara.rakotomavo@gmail.com>
2019-10-24 18:12:44 +0200

    Recuperer le nom des reads

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -9,4 +9,12 @@ mkdir Cleaning-Trimming-outputs
 
 gunzip *.gz
 
+# Récuperer le nom des reads
+for i in $(ls fastq/*_R1.fastq);do
+echo $i;
+Read1="$i";
+echo $nameR1;
+Read2=$(echo $i | sed s/R1/R2/g);
+echo $Read2;
+
 
