dc6e981d77c16ae7c471d56a566a4a091f24a195
miara1502 <miara.rakotomavo@gmail.com>
2019-10-24 18:28:35 +0200

    mergind des reads

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -21,6 +21,18 @@ echo $Read2;
 java -jar ./soft/AlienTrimmer.jar -if $Read1 -ir $Read2 -c ./databases/contaminants.fasta -q 20 -of ./result1/$(basename $Read1) -or ./result1/$(basename $Read2)
 done
 
-#Merging 
+#Merging des Read1 et Read2
+
+mkdir result_merge
+
+for i in $(ls result1/*_R1.fastq);do
+vsearch --fastq_mergepairs $i --reverse ${i:0:-9}"_R2.fastq" --fastqout ./result_merge/$(basename ${i:0:-9})".fasta" --label_suffix $(basename ${i:0:-9})
+done
+
+for i in $(ls result_merge/*.fasta);do
+cat $i | sed -e 's/ //g' > result_merge/amplicon.fasta
+done
+
+
 
 
